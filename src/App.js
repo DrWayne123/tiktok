98b26019284a86d62a875208913d1f545dfecbaf
dr.waynegit config --global user.email <Qladu1994@gmail.com>
2022-09-01 16:21:17 +0700

    cấu hình để import fie cũng như gọi ý file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ád á ád
+          Learn React ád á ád s s ss ss s
         </a>
       </header>
     </div>
