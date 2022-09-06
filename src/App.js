1b449ecafc2970d9d47a494d19353abd93ed5ca3
dr.waynegit config --global user.email <Qladu1994@gmail.com>
2022-09-06 11:08:06 +0700

    update css search and logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/Routes';
 import { DefaultLayout } from '~/components/Layout';
-import classNames from 'classnames/bind';
 
 function App() {
     return (
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,43 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>Header</div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok"></img>
+                </div>
+
+                <div className={cx('search')}>
+                    <input
+                        className={cx('input')}
+                        placeholder="Search accounts and videos"
+                    ></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                        <FontAwesomeIcon
+                            icon={faSpinner}
+                            className={cx('loading')}
+                        />
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
